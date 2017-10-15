0e827d4110d1b87c86f69027b003e4fbbafd6b8d
makotoOkamura <makoto@makotoOkamura-no-MacBook.local>
2017-10-15 14:39:04 +0900

    change race settings

--- run_samurai.sh
+++ run_samurai.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 
-DATETIME=`date +%Y%m%d%H%M%S`
-
 function install_samurai () {
     wget http://samuraicoding.info/software/samurai-software-20170930.zip
     unzip -o samurai-software-20170930.zip
@@ -17,6 +15,7 @@ function create_make_players () {
 
 function create_race_settings () {
     {
+        echo 'DATETIME=`date +%Y%m%d%H%M%S`'
         echo 'COURSE=samples/sample-course.smrjky'
         echo 'AI1=player/greedy'
         echo 'AI1_NAME=ai1'
@@ -24,6 +23,7 @@ function create_race_settings () {
         echo 'AI2_NAME=ai2'
         echo 'LOG1=log/$DATETIME$AI1_NAME.log'
         echo 'LOG2=log/$DATETIME$AI2_NAME.log'
+        echo 'RACELOG=log/$DATETIME.racelog'
     } > race_settings.txt
     chmod 777 make_players.sh
 }
@@ -43,6 +43,6 @@ fi
 mkdir -p log
 ./make_players.sh
 . race_settings.txt
-official/official $COURSE $AI1 $AI1_NAME $AI2 $AI2_NAME $LOG1 $LOG2 > ./log/$DATETIME.racelog
+official/official $COURSE $AI1 $AI1_NAME $AI2 $AI2_NAME $LOG1 $LOG2 > $RACELOG
 
 /bin/sh
